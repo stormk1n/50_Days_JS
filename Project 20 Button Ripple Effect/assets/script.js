e8cc3a8c3349f32fa03eb81b7272afcda1eecbe4
stormk1n <173473111+stormk1n@users.noreply.github.com>
2026-09-10 09:30:21 +0100

    fix: make click in button only - project 20

--- Project 20 Button Ripple Effect/assets/script.js
+++ Project 20 Button Ripple Effect/assets/script.js
@@ -1,2 +1,16 @@
-const button = document.querySelectorAll('.ripple')
+const buttons = document.querySelectorAll('.ripple')
 
+buttons.forEach(button => {
+    button.addEventListener('click', (e) =>{
+        const x = e.clientX
+        const y = e.clientY
+
+        const buttonTop = e.target.offsetTop
+        const buttonLeft = e.target.offsetLeft
+
+        const xInside = x - buttonLeft
+        const yInside = y - buttonTop
+
+        console.log(xInside, yInside)
+    })
+})
