9b6458dafd165967af16eab618a5fb7d837ddd40
stormk1n <173473111+stormk1n@users.noreply.github.com>
2026-09-10 12:13:47 +0100

    init: start js for drag start

---
+++ Project 21 Drag n Drop/assets/script.js
@@ -0,0 +1,32 @@
+const fill = document.querySelector('.fill')
+const empties = document.querySelectorAll('.empty')
+
+
+fill.addEventListener('dragstart', dragStart)
+fill.addEventListener('dragend', dragEnd)
+
+
+function dragStart(){
+    console.log('drag start')
+}
+
+
+function dragEnd(){
+    console.log('drag end')
+}
+
+function dragOver(){
+    console.log('drag over')
+}
+
+function dragEnter(){
+    console.log('drag enter')
+}
+
+function dragLeave(){
+    console.log('drag leave')
+}
+
+function dragDrop(){
+    console.log('drag drop')
+}
