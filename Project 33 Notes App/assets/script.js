23a869b2355bfa4286ef999365dda7dc63ff93ea
stormk1n <173473111+stormk1n@users.noreply.github.com>
2026-09-23 12:14:15 +0100

    feat: add local storage functionality

---
+++ Project 33 Notes App/assets/script.js
@@ -0,0 +1,68 @@
+const addBtn = document.getElementById("add")
+
+const notes = JSON.parse(localStorage.getItem('notes'))
+
+if (notes){
+    notes.forEach(note => addNewNote(note))
+}
+
+addBtn.addEventListener('click', () => addNewNote('# Hello Hacker!'))
+
+function addNewNote(text = '') {
+    const note = document.createElement('div')
+    note.classList.add('note')
+
+    note.innerHTML = `
+        <div class="tools">
+            <button class="edit">
+                <span class="material-icons">edit</span>
+            </button>
+            <button class="delete">
+                <span class="material-icons">delete</span>
+            </button>
+        </div>
+
+        <div class="main ${text ? "" : 'hidden'}"></div>
+        <textarea class="${text ? "hidden" : ""}"></textarea>
+        `
+    const editBtn = note.querySelector('.edit')
+    const deleteBtn = note.querySelector('.delete')
+    const main = note.querySelector('.main')
+    const textArea = note.querySelector('textarea')
+
+    textArea.value = text
+    main.innerHTML = marked(text)
+
+    deleteBtn.addEventListener('click', () => {
+        note.remove()
+
+        updateLS()
+    })
+
+    editBtn.addEventListener('click', () => {
+        main.classList.toggle('hidden')
+        textArea.classList.toggle('hidden')
+    }
+    )
+
+    textArea.addEventListener('input', (e) => {
+        const {value} = e.target
+
+        main.innerHTML = marked(value)
+
+        updateLS()
+    })
+
+    document.body.appendChild(note)
+}
+
+
+function updateLS(){
+    const notesText = document.querySelectorAll('textarea')
+
+    const notes = []
+    
+    notesText.forEach(note => notes.push(note.value))
+
+    localStorage.setItem('notes', JSON.stringify(notes))
+}
